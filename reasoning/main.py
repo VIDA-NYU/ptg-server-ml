e77b0e49451377cc682dc7fc703331fe0eb5952a
Roque Lopez <rlopezc27@gmail.com>
2022-09-02 15:56:15 -0400

    Read output from perception

--- reasoning/main.py
+++ reasoning/main.py
@@ -19,21 +19,13 @@ configs = {'rule_classifier_path': '/src/app/recipe_tagger',
 class ReasoningApp:
 
     def __init__(self):
-        self.api = ptgctl.CLI(
-            username=os.getenv('API_USER') or 'yolo',
-            password=os.getenv('API_PASS') or 'yolo')
+        self.api = ptgctl.CLI(username=os.getenv('API_USER') or 'reasoning',
+                              password=os.getenv('API_PASS') or 'reasoning')
 
     @ptgctl.util.async2sync
-    async def run(self):
-        input_sid = 'clip:basic-zero-shot:instructions'
-
-        step_id_repr = 'reasoning:step_id'
-        step_status_repr = 'reasoning:step_status'
-        step_description_repr = 'reasoning:step_description'
-        error_status_repr = 'reasoning:error_status'
-        error_description_repr = 'reasoning:error_description'
-
-        output_sids = [step_id_repr, step_status_repr, step_description_repr, error_status_repr, error_description_repr]
+    async def run(self, prefix=None):
+        prefix = prefix or ''
+        input_sid = f'{prefix}clip:action:steps'
 
         recipe_id: str = self.api.sessions.current_recipe()
         recipe = self.api.recipes.get(recipe_id)
@@ -43,28 +35,20 @@ class ReasoningApp:
         logger.info('First step: %s' % str(step_data))
 
         async with self.api.data_pull_connect(input_sid) as ws_pull, \
-                   self.api.data_push_connect(output_sids) as ws_push:
+                   self.api.data_push_connect([f'{prefix}reasoning'], batch=True) as ws_push:
             while True:
-                print('Inside')
                 data = ptgctl.holoframe.load_all(await ws_pull.recv_data())
-                logger.info('Data  from Hololens: %s' % str(data))
                 action_pred = data[input_sid]
+                action_pred.pop('timestamp', None)
+                action_pred.pop('frame_type', None)
                 action_text, action_prob = zip(*action_pred.items())
                 logger.info('Perception outputs: %s' % str(action_text))
                 i_topk = np.argpartition(action_prob, 5)
                 i_topk = sorted(i_topk, key=lambda i: action_prob[i], reverse=True)
-
                 results = state_manager.check_status([action_text[i] for i in i_topk])
+                logger.info('Reasoning outputs: %s' % str(results))
 
-                await ws_push.send_data(
-                    [
-                        orjson.dumps(results['step_id']),
-                        orjson.dumps(results['step_status']),
-                        orjson.dumps(results['step_description']),
-                        orjson.dumps(results['error_status']),
-                        orjson.dumps(results['error_description'])
-                    ]
-                )
+                await ws_push.send_data([orjson.dumps(results)])
 
 
 if __name__ == '__main__':
