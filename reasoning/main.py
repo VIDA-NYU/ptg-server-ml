8a4bc5f63abc5823baf693a4d3b246a32003d25d
Roque Lopez <rlopezc27@gmail.com>
2022-09-08 17:10:13 -0400

    Select correctly the top-k actions

--- reasoning/main.py
+++ reasoning/main.py
@@ -1,10 +1,10 @@
 import os
+import time
 import orjson
 import logging
 import ptgctl
 import ptgctl.holoframe
 import ptgctl.util
-import numpy as np
 from tim_reasoning import StateManager
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(name)s:%(message)s')
@@ -33,19 +33,18 @@ class ReasoningApp:
         state_manager = StateManager(configs)
         step_data = state_manager.start_recipe(recipe)
         logger.info('First step: %s' % str(step_data))
-
+        top = 5
         async with self.api.data_pull_connect(input_sid) as ws_pull, \
                    self.api.data_push_connect([f'{prefix}reasoning'], batch=True) as ws_push:
             while True:
+                time.sleep(2)  # Sleep n seconds
                 data = ptgctl.holoframe.load_all(await ws_pull.recv_data())
-                action_pred = data[input_sid]
-                action_pred.pop('timestamp', None)
-                action_pred.pop('frame_type', None)
-                action_text, action_prob = zip(*action_pred.items())
-                logger.info('Perception outputs: %s' % str(action_text))
-                i_topk = np.argpartition(action_prob, 5)
-                i_topk = sorted(i_topk, key=lambda i: action_prob[i], reverse=True)
-                results = state_manager.check_status([action_text[i] for i in i_topk])
+                action_predictions = data[input_sid]
+                action_predictions.pop('timestamp', None)
+                action_predictions.pop('frame_type', None)
+                top_actions = sorted(action_predictions.items(), key=lambda x: x[1], reverse=True)[:top]
+                logger.info('Perception outputs: %s' % str(top_actions))
+                results = state_manager.check_status([i[0] for i in top_actions])
                 logger.info('Reasoning outputs: %s' % str(results))
 
                 await ws_push.send_data([orjson.dumps(results)])
