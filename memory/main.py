df018184424b51b38da8aacbdaeb5dcd2c319f70
Bea <bea.steers@gmail.com>
2022-09-13 14:34:31 -0400

    add 3d memory app

---
+++ memory/main.py
@@ -0,0 +1,43 @@
+import os
+import orjson
+import logging
+import numpy as np
+import ptgctl
+import ptgctl.holoframe
+import ptgctl.util
+import re_id
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(name)s:%(message)s')
+logger = logging.getLogger(__name__)
+
+#ptgctl.log.setLevel('WARNING')
+
+class MemoryApp:
+    def __init__(self):
+        self.api = ptgctl.API(username=os.getenv('API_USER') or 'memory',
+                              password=os.getenv('API_PASS') or 'memory')
+        self.re_id = re_id.ReId()
+
+    @ptgctl.util.async2sync
+    async def run(self, prefix=None):
+        prefix = prefix or ''
+        input_sid = f'{prefix}detic:world'
+        output_sid = f'{prefix}detic:world:memory'
+
+        async with self.api.data_pull_connect(input_sid) as ws_pull, \
+                   self.api.data_push_connect(output_sid, batch=True) as ws_push:
+            while True:
+                for sid, timestamp, data in await ws_pull.recv_data():
+                    objects = orjson.loads(data)
+
+                    for obj in objects:
+                        label, seen_before = self.re_id.update_memory(np.asarray(obj['xyz_center']), obj['labels'])
+                        obj['track_id'] = label
+                        obj['seen_before'] = seen_before
+
+                    await ws_push.send_data([orjson.dumps(objects)])
+
+
+if __name__ == '__main__':
+    import fire
+    fire.Fire(MemoryApp)
---
+++ memory/re_id.py
@@ -0,0 +1,115 @@
+"""
+Author Jianzhe Lin
+May.2, 2020
+"""
+from collections import defaultdict
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+
+class ReId:
+    MIN_DEPTH_POINT_DISTANCE = 7
+
+    def __init__(self) -> None:
+        self.location_memory = {}
+        self.instance_count = defaultdict(lambda: 0)
+
+    def update_memory(self, xyz, label):
+        # check memory
+        for k, xyz_seen in self.location_memory.items():
+            if label == k and self.memory_comparison(xyz_seen, xyz):
+                return k, True
+            elif label == k[:-2] and self.memory_comparison(xyz_seen, xyz):
+                return k, True
+        # unique name for multiple instances
+        if label in self.location_memory:
+            self.instance_count[label] += 1
+            i = self.instance_count[label]
+            label = f'{label}_{i}'
+        
+        # TODO: add other info
+        self.location_memory[label] = xyz
+        return label, False
+
+    def memory_comparison(self, seen, candidate):
+        '''Compare a new instance to a previous one. Determine if they match.'''
+        return np.linalg.norm(candidate - seen) < 2
+
+
+class DrawResults:
+    def __init__(self, memory):
+        self.location_memory = memory
+
+    def draw_4panel(self, results, rgb):
+        res = results.imgs[0]
+        return np.vstack([
+            np.hstack([
+                self.draw_memory_yolo(results, rgb), 
+                self.draw_basic_yolo(results),
+            ]), 
+            np.hstack([
+                self.draw_message_board(res.shape), 
+                self.draw_3d_space(res.shape),
+            ]),
+        ])
+
+    def draw_memory_yolo(self, results, rgb):
+#        img = results.imgs[0]
+        for b, seen in zip(results.xywh[0], results.seen_before):
+            rgb = draw_bbox(
+                rgb.copy(), *b[:4], 
+                color=(255, 0, 0) if seen else (0, 255, 0))
+        draw_text_list(rgb, [
+            f'hey I remember {name}'
+            for name, seen in zip(results.track_ids, results.seen_before)
+            if seen
+        ])
+        return rgb.copy()
+
+
+    def draw_basic_yolo(self, results):
+        return results.render()[0]
+
+    def draw_message_board(self, shape):
+        img = np.ones(shape, np.uint8) * 255
+        img = draw_text_list(img, [
+            f"{name}: [{', '.join(f'{x:.3f}' for x in loc)}]"
+            for name, loc in self.location_memory.items()
+        ])
+        return img
+
+    def draw_3d_space(self, shape):
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+
+        # ax.set_xlim((-2, 0))
+        # ax.set_ylim((-2, 0))
+        # ax.set_zlim((-2, 0))
+
+        for name, loc in self.location_memory.items():    
+            ax.scatter(*loc, marker='^')   
+            ax.text(*loc, name, fontsize=6)
+
+        fig.canvas.draw()
+        src = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+        src = src.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        src = cv2.resize(src, shape[:2][::-1])
+        plt.close()
+        return src
+
+# drawing
+
+def draw_text_list(img, texts):
+    for i, txt in enumerate(texts):
+        cv2.putText(img, txt, (20+400*(i//12), 40+30*(i%12)), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 48, 48), 2)
+    return img
+
+
+def draw_bbox(img, xc, yc, w, h, *, color=(0, 255, 0)):
+    img = cv2.rectangle(
+        img, 
+        (int(xc - w/2), int(yc - h/2)), 
+        (int(xc + w/2), int(yc + h/2)), 
+        color, 2)
+    return img
