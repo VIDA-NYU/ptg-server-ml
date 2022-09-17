54a522ed71892f0af9cbee03ec60db2218d8ef0b
Bea Steers <bea.steers@gmail.com>
2022-09-17 09:56:10 -0400

    recording fixes

--- ptgprocess/record.py
+++ ptgprocess/record.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 import os
+import glob
+import tqdm
 import orjson
 import numpy as np
 from .util import Context
@@ -21,23 +23,32 @@ class RawWriter(BaseWriter):
     def context(self, sample, t_start):
         import zipfile
         print("Opening zip file:", self.fname)
-        with zipfile.ZipFile(self.fname, 'a', zipfile.ZIP_STORED, False) as self.writer:
-            yield self
+        try:
+            with zipfile.ZipFile(self.fname, 'a', zipfile.ZIP_STORED, False) as self.writer:
+                yield self
+        finally:
+            print('closing', self.fname)
 
     def write(self, data, ts):
         self.writer.writestr(ts, data)
 
 
 class VideoWriter(BaseWriter):
-    def __init__(self, name, store_dir, sample, t_start, fps=15, vcodec='libx264', crf='23',  **kw):
+    def __init__(self, name, store_dir, sample, t_start, fps=15, vcodec='libx264', crf='23', scale=None, norm=None, max_duplicate_secs=10,  **kw):
         super().__init__(**kw)
-        fname = os.path.join(store_dir, f'{name}.mp4')
+        self.fname = fname = os.path.join(store_dir, f'{name}.mp4')
+
+        if name == 'depthlt':
+            scale = 40 if scale is None else scale
+        self.scale = scale
+        self.norm = norm
         
         self.prev_im = self.dump(sample['image'])
         self.t_start = t_start
         h, w = sample['image'].shape[:2]
 
         self.fps = fps
+        self.max_duplicate = max_duplicate_secs * fps
         self.cmd = (
             f'ffmpeg -y -s {w}x{h} -pixel_format bgr24 -f rawvideo -r {fps} '
             f'-i pipe: -vcodec {vcodec} -pix_fmt yuv420p -crf {crf} {fname}')
@@ -65,9 +76,19 @@ class VideoWriter(BaseWriter):
             if process.stdin:
                 process.stdin.close()
             process.wait()
-            print('finished')
+            print('finished', self.fname)
 
     def dump(self, im):
+        if self.scale:
+            im = (im * self.scale).astype(im.dtype)
+        if self.norm:
+            im = im / im.max()
+
+        # convert int32 to uint8
+        if not np.issubdtype(im.dtype, np.uint8):
+            if np.issubdtype(im.dtype, np.integer):
+                im = im.astype(float) / np.iinfo(im.dtype).max
+            im = (im * 255).astype(np.uint8)
         if im.ndim == 2:
             im = np.broadcast_to(im[:,:,None], im.shape+(3,))
         return im[:,:,::-1].tobytes()
@@ -75,9 +96,14 @@ class VideoWriter(BaseWriter):
     def write(self, data, ts=None):
         im = self.dump(data['image'])
         if ts is not None:
+            #i = 0
             while self.t < ts - self.t_start:
                 self.writer.write(self.prev_im)
                 self.t += 1.0 / self.fps
+                #i += 1
+                #if i > self.max_duplicate:
+                #    self.t = ts
+                #    break
             self.prev_im = im
         self.writer.write(im)
         self.t += 1.0 / self.fps
@@ -122,6 +148,7 @@ class JsonWriter(BaseWriter):
                 yield self
             finally:
                 self.fh.write(b'\n]\n')
+                print('closing json', self.fname, flush=True)
 
     def write(self, d, ts=None):
         if self.i:
@@ -161,6 +188,7 @@ class CsvWriter(BaseWriter):
 
 
 class RawReader:
+    reader = None
     def __init__(self, src):
         if os.path.isdir(src):
             fs = sorted(glob.glob(os.path.join(src, '*')))
@@ -178,8 +206,11 @@ class RawReader:
         import zipfile
         for f in self.fs:
             with zipfile.ZipFile(f, 'r', zipfile.ZIP_STORED, False) as self.reader:
-                for ts in sorted(self.reader.namelist()):
-                    with zf.open(ts, 'r') as f:
+                fs = sorted(self.reader.namelist())
+                pbar = tqdm.tqdm(fs)
+                for ts in pbar:
+                    pbar.set_description(f'{ts}')
+                    with self.reader.open(ts, 'r') as f:
                         yield ts, f.read()
             self.reader = None
 
