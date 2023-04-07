044f55511c05dcb94ebd5b9feda2fa0d75e78aff
nyu bbn machine <bea.steers@gmail.com>
2023-04-07 11:52:03 -0500

    merge

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Download extra resources
-pip show en_core_web_lg || python -m spacy download en_core_web_lg
-python -c "import nltk;nltk.download('punkt')"
-
-
 maybe_download_model() {
 	FID=$REASONING_MODELS_PATH/.${1}.google_drive_id
         [ "$([ -f "$FID" ] && cat "$FID")" != "${2}" ] \
@@ -16,6 +11,7 @@ download_model() {
         gdown "${2}" -O "$ZF" && unzip "$ZF" -d $REASONING_MODELS_PATH && rm "$ZF"
 }
 
-maybe_download_model recipe_tagger 1aYSlngadawRTKuIkd1FtMvrMBfenqPLH
+maybe_download_model epic_mir_plus 1Vd8Q-Myj2lgkZrmyImCjZlcJjGpZ3Zff
+maybe_download_model egovlp_fewshot 1eqASv2bU1VnVf2qK8_prCn9HNXdK35d3
 
 python main.py run $@
--- detic/entrypoint.sh
+++ detic/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Download extra resources
-pip show en_core_web_lg || python -m spacy download en_core_web_lg
-python -c "import nltk;nltk.download('punkt')"
-
-
 maybe_download_model() {
 	FID=$REASONING_MODELS_PATH/.${1}.google_drive_id
         [ "$([ -f "$FID" ] && cat "$FID")" != "${2}" ] \
@@ -16,6 +11,6 @@ download_model() {
         gdown "${2}" -O "$ZF" && unzip "$ZF" -d $REASONING_MODELS_PATH && rm "$ZF"
 }
 
-maybe_download_model recipe_tagger 1aYSlngadawRTKuIkd1FtMvrMBfenqPLH
+maybe_download_model egohos 1m7C_rWymUi045CpWkaXza8zWJ-Emys1X
 
 python main.py run $@
